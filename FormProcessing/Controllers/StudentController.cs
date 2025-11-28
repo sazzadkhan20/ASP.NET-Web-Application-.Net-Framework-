cbf2ed0439eeba98e7950b2bc1c07a1094297195
sazzadkhan20 <sazzad53913@gmail.com>
2025-11-29 02:48:54 +0600

    Form Processing Done

---
+++ FormProcessing/Controllers/StudentController.cs
@@ -0,0 +1,69 @@
+﻿using FormProcessing.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormProcessing.Controllers
+{
+    public class StudentController : Controller
+    {
+        public static List<Student> students = new List<Student>() { };
+        public void addStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Model Binding
+        [HttpPost]
+        public ActionResult Index(Student student)
+        {
+            if(student != null)
+            {
+                students.Add(student);
+                TempData["StudentCreate"] = "New Student Created";
+                // ViewBag or ViewData Not accepting Here
+                return RedirectToAction("Details");
+            }
+            return View();
+        }
+
+        //[HttpGet]
+        public ActionResult Details()
+        {
+            return View(students);
+        }
+
+
+        // Variable Name Mapping Form Processing
+        //[HttpPost]
+        //public ActionResult Index(string ID,string UserName)
+        //{
+        //    ViewBag.name = UserName;
+        //    return View();
+        //}
+
+        // FormCollection Object Form Processing
+        //[HttpPost]
+        //public ActionResult Index(FormCollection fc)
+        //{
+        //    ViewBag.name = fc["UserName"];
+        //    return View();
+        //}
+
+
+        // Request Form Processing
+        //public ActionResult Index()
+        //{
+        //    ViewBag.name = Request["UserName"];
+        //    return View();
+        //}
+    }
+}
---
+++ FormProcessing/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormProcessing.Models
+{
+    public class Student
+    {
+        public string ID { get; set; }
+        public string UserName { get; set; }
+        public string Email {  get; set; }
+    }
+}
