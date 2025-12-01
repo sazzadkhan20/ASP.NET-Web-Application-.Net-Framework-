8715c47284ed4e4f7a4099052ea1eaacbc786ac1
sazzadkhan20 <sazzad53913@gmail.com>
2025-12-01 13:47:13 +0600

    Entity Build Up Done

---
+++ EntityFrameworkDBFirstCrudOperation/Controllers/RegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using EntityFrameworkDBFirstCrudOperation.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkDBFirstCrudOperation.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        TestDBEntities db = new TestDBEntities();
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View(new Student());
+        }
+
+        [HttpPost]
+        public ActionResult Register(Student s,string Gaming,string Sports,string Travelling)
+        {
+            string hobbies = "";
+            if (Gaming != null) hobbies += Gaming + ", ";
+            if (Sports != null) hobbies += Sports + ", ";
+            if (Travelling != null) hobbies += Travelling + ", ";
+            s.Hobbies = hobbies;
+            db.Students.Add(s); // Insert Operation
+            db.SaveChanges(); // Return No. of Rows affected
+            TempData["StudentCreate"] = "New Student Created";
+            return RedirectToAction("Home","Student");
+        }
+    }
+}
---
+++ EntityFrameworkDBFirstCrudOperation/Controllers/StudentController.cs
@@ -0,0 +1,90 @@
+﻿using EntityFrameworkDBFirstCrudOperation.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFrameworkDBFirstCrudOperation.Controllers
+{
+    public class StudentController : Controller
+    {
+        TestDBEntities db = new TestDBEntities();
+        [HttpGet]
+        public ActionResult Home()
+        {
+            // List<Student> students = db.Students.ToList();
+            var students = db.Students.ToList(); // Fetching All Rows
+            return View(students);
+        }
+
+        //[HttpPost]
+        //public ActionResult Home()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Details()
+        //{
+        //    return View(new Student());
+        //}
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            Student s = db.Students.Find(id); // Return one Row 
+            return View(s);
+        }
+
+        //[HttpGet]
+        //public ActionResult Edit()
+        //{
+        //    return View(new Student());
+        //}
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Student s = db.Students.Find(id);
+            return View(s);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+            var student = db.Students.Find(s.ID);
+            student.Name = s.Name; // Update Operation
+            student.Email = s.Email;
+            student.Phone = s.Phone;
+            student.NID = s.NID;
+            student.Profession = s.Profession;
+            student.Password = s.Password;
+            db.SaveChanges();
+            TempData["DataUpdate"] = "Update Successfull";
+            return RedirectToAction("Home");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Student s = db.Students.Find(id);
+            if(s != null)
+            {
+                db.Students.Remove(s); // Delete Operation
+                db.SaveChanges();
+                TempData["StudentDelete"] = "Student Deleted"; 
+            }
+            return RedirectToAction("Home");
+        }
+
+        public ActionResult Search(string id)
+        {
+            var student = (from s in db.Students
+                           where s.Name.Contains(id)
+                           orderby s.ID descending
+                           select s).ToList();
+            return View(student);
+        }
+
+    }
+}
