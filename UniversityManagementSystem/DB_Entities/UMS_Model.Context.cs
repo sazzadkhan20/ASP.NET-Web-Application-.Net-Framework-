7e2b7842076084216e498e4f80ba9dbc50bebd60
sazzadkhan20 <sazzad53913@gmail.com>
2026-01-19 00:50:26 +0600

    Add Feature in project

--- UniversityManagementSystem/DB_Entities/UMS_Model.Context.cs
+++ UniversityManagementSystem/DB_Entities/UMS_Model.Context.cs
@@ -18,6 +18,7 @@ namespace UniversityManagementSystem.DB_Entities
         public UMS_DBEntities()
             : base("name=UMS_DBEntities")
         {
+            // this.Configuration.LazyLoadingEnabled = false;  
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
